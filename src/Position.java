1b91e0224205eaa0c07cb046361a18f2a864011b
Muneer Kattubadi <mkatt@ufl.edu>
2017-11-26 20:26:41 -0500

    initial commit

---
+++ src/Position.java
@@ -0,0 +1,10 @@
+class Position{
+private int row, col;
+Position(){;}
+Position(int row, int col){
+this.row = row;
+this.col = col;
+}
+public int getRow() {return row;}
+public int getCol() {return col;}
+}
